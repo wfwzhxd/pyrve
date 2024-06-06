10966daf3c53ec438399188750e2a3fbd0e925af
Your Name <you@example.com>
2024-06-06 17:08:15 +0800

    simple memory

--- main_memory.py
+++ main_memory.py
@@ -2,13 +2,20 @@
 class Memory:
     
     def __init__(self, start, end) -> None:
-        pass
+        self.mem = bytearray(end-start+1)
+        self.base = start
 
     def read(self, address, length):
         '''
             return unsigned byte array
         '''
-        pass
+        if address<self.base or address+length>self.base+len(self.mem):
+            raise IndexError("read {} at address{} not valid in mem[{}:{}]".format(length, address, self.base, self.base+len(self.mem)))
+        address -= self.base
+        return self.mem[address:address+length]
 
     def write(self, address, data:bytes):
-        pass
+        if address<self.base or address+len(data)>self.base+len(self.mem):
+            raise IndexError("write {} at address{} not valid in mem[{}:{}]".format(len(data), address, self.base, self.base+len(self.mem)))
+        address -= self.base
+        self.mem[address:address+len(data)] = data
